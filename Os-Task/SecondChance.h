e5433d11cef9fb4510c155bf01a368bae52f08ba
Hassan Ibrahim <35563086+HassanIbrahimfatooh@users.noreply.github.com>
2018-03-26 14:58:22 +0200

    Update SecondChance.h

--- Os-Task/SecondChance.h
+++ Os-Task/SecondChance.h
@@ -1,12 +1,93 @@
-#ifndef SECONDCHANCE_H_INCLUDED
-#define SECONDCHANCE_H_INCLUDED
+#ifndef SecondChance_H_INCLUDED
+#define SecondChance_H_INCLUDED
 
 using namespace std;
 
 // SecondChance function
-void SecondChance(int Pages[], int NPages, int  NFrames){
-    // Complete this function
+void SecondChance(int P[],int  NPages,int NFrames)
+{
+    int Pages[18] = {0,4,1,4,2,4,3,4,2,4,0,4,1,4,2,4,3,4};
+    int *frames = new int [NFrames] ;    /* array for frames */
+    bool *secondChanceBit = new bool[NFrames] ;  /*SECOND CHANCE Bit */
+    bool valid[10];
+    int frame = 0; /* index of the next frame to add pages in */
+    bool done; /* check if page find frame */
+    int totalMiss = 0;
+
+    //initialize frames as empty
+    for (int i=0; i<NFrames; i++)
+    {
+        frames[i]= -1;
+        secondChanceBit[i] = false;
+    }
+    // initialize all valid with 0 for expected pages 1-10
+    for (int i=0; i<10; i++)
+        valid[i] = false;
+    for (int readyPage=0; readyPage<NPages; readyPage++)
+    {
+        do
+        {
+            if (frames[frame]==-1 && valid[Pages[readyPage]-1]== false)
+            {
+                cout<<"first condition";
+
+                frames[frame]=Pages[readyPage];
+                valid[Pages[readyPage]-1] = true;
+                secondChanceBit[frame] = false;
+                cout<<"you are at frame "<< frame;
+                frame = (frame+1)%NFrames;
+
+            }
+            else if (valid[Pages[readyPage]-1] == true)
+            {
+
+                cout<<"second condition";
+
+                cout<<"you are at frame "<< frame;
+                for (int i =0; i<NFrames; i++)
+                {
+                    if (Pages[readyPage]==frames[i])
+                        secondChanceBit[i]=true;
+                }
+            }
+            else if (secondChanceBit[frame]== false)
+            {
+                cout<<"fourth condition";
+                cout<<"you are at frame "<< frame;
+                valid[frames[frame]-1] = false;
+                frames[frame]=Pages[readyPage];
+                secondChanceBit[frame] = false;
+                frame = (frame+1)%NFrames;
+                valid[Pages[readyPage]-1] = true;
+                totalMiss++;
+
+
+
+
+            }
+            else if (secondChanceBit[frame]== true)
+            {
+                cout<<"third condition" <<"\n";
+                cout<<"you are at frame "<< frame;
+                secondChanceBit[frame] = false;
+                frame = (frame+1)%NFrames;
+
+
+
+            }
+
+
+
+        }
+        while (valid[Pages[readyPage]-1] == false);
+
+
+        cout << "total miss: "<<totalMiss<< "\n";
+        for (int qq = 0 ; qq<NFrames; qq++)
+            cout<< frames[qq]<<" ";
+        cout<< "\n";
+    }
 }
 
-
-#endif // SECONDCHANCE_H_INCLUDED
+#endif // SECONDC#ifndef SECONDCHANCE_H_INCLUDED
+#define SecondChance_H_INCLUDED
