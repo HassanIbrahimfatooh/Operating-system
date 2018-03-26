580d7628e858ae799ef84dff904cda81d02a1f80
Hassan Ibrahim <35563086+HassanIbrahimfatooh@users.noreply.github.com>
2018-03-26 14:57:50 +0200

    Update Optimal.h

--- Os-Task/Optimal.h
+++ Os-Task/Optimal.h
@@ -5,32 +5,32 @@ using namespace std;
 
 // Optimal function
 void Optimal(int Pages[], int NPages, int  NFrames){
-   
+
     // Frames Array
     int *Frames;
     Frames = new int [NFrames];
     for(int i=0; i< NFrames; i++)
         Frames[i] = -1; // Empty Frame
-    
+
     int TotalMiss = 0; // Total Miss Counter
-    
+
     // Loop on Pages
     for(int i=0; i<NPages; i++)
     {
         bool isThereEmptyFrame = false;
         bool isPageAlreadyPresented = false;
-        
+
         // Loop on Frames
         for(int j=0; j<NFrames; j++)
         {
-            
+
             // Check if the Page is aleardy presented
             if(Frames[j] == Pages[i])
             {
                 isPageAlreadyPresented = true;
                 break;
             }
-            
+
             // Check if there is Empty Frame
             else if(Frames[j] == -1)
             {
@@ -40,19 +40,19 @@ void Optimal(int Pages[], int NPages, int  NFrames){
                 break;
             }
         } // End of Loop on Frames
-        
+
         // Need to Replace
         if((!isThereEmptyFrame) && (!isPageAlreadyPresented))
         {
             TotalMiss++;
             int MaxDistance = 0;
             int Index = -1;
-            
+
             // Loop on Frames
             for(int j=0; j< NFrames; j++)
             {
                 bool isPageUsedInFuture = false;
-                
+
                 // Loop on Future use Pages
                 for(int k=i+1; k<NPages; k++)
                 {
@@ -60,7 +60,7 @@ void Optimal(int Pages[], int NPages, int  NFrames){
                     if(Frames[j] == Pages[k])
                     {
                         isPageUsedInFuture = true;
-                        
+
                         if((k - i) > MaxDistance)
                         {
                             MaxDistance = k - i;
@@ -69,7 +69,7 @@ void Optimal(int Pages[], int NPages, int  NFrames){
                         break;
                     }
                 } // End Loop on Future use Pages
-                
+
                 if(!isPageUsedInFuture)
                 {
                     MaxDistance = NPages; // The Biggest Value forever
@@ -77,20 +77,20 @@ void Optimal(int Pages[], int NPages, int  NFrames){
                     break;
                 }
             } // End of Loop on Frames
-            
-            // Replace The Frame's Page 
+
+            // Replace The Frame's Page
             Frames[Index] = Pages[i];
         }
-        
-        // Show Frames 
+
+        // Show Frames
         for(int j=0;j<NFrames;j++)
         {
             cout<<Frames[j]<<" ";
         }
-        cout<<endl;  
-        
+        cout<<endl;
+
     } // End of Loop on Pages
-    
+
     // Show Tota Miss
     cout<<"Total Miss = "<<TotalMiss<<endl;
 }
